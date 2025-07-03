1007db82f8b14d0513551e0da6b839407d26b58f
Omar Reda <omar.reda13231@gmail.com>
2025-07-03 03:02:41 +0300

    App up and running

--- app.py
+++ app.py
@@ -4,31 +4,34 @@ import streamlit as st
 import requests
 import numpy as np
 
+
 def fetch_poster(movie_id):
     response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=c3c76c97b08eb613d4e98a165139e2e8".format(movie_id))
     data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data["poster_path"]
+
+
 def recommend(movie):
-    movie_index = movies[movies["title"] == movie].index[0]
-    distances = similarity[movie_index]
+    movieIndex = movies[movies["title"] == movie].index[0]
+    distances = similarity[movieIndex]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
-    recommended_movies = []
-    recommended_movies_posters = []
+    recommendedMovies = []
+    recommendedMoviesPosters = []
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
-        recommended_movies.append(movies.iloc[i[0]].title)
-        recommended_movies_posters.append(fetch_poster(movie_id))
-    return recommended_movies, recommended_movies_posters
+        recommendedMovies.append(movies.iloc[i[0]].title)
+        recommendedMoviesPosters.append(fetch_poster(movie_id))
+    return recommendedMovies, recommendedMoviesPosters
 
 
 movies = pd.read_csv("movies.csv")
 similarity = np.load('similarity.npz')['similarity']
 
-movie_titles = movies["title"].values
+movieTitles = movies["title"].values
 
 st.title("Movie Recommender System")
-selected_movie_name = st.selectbox("Search for a movie", movie_titles)
+selected_movie_name = st.selectbox("Search for a movie", movieTitles)
 
 if st.button("Recommend"):
     names, posters = recommend(selected_movie_name)
